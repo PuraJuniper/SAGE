e14eb413594ca17d926a26e3bac6c777a77efed0
Winston Leung <winstonleung@Winstons-MacBook-Pro.local>
2021-10-06 15:01:49 -0400

    changed footer

--- src/footer.js
+++ src/footer.js
@@ -14,7 +14,7 @@ class Footer extends React.Component {
 	render() {
 		return <div className="row footer">
 			<div className="col-xs-12">
-				FRED is project of <a href="https://smarthealthit.org" target="_blank">SMART Health IT</a> and the open source code for the app is available on <a href="https://github.com/smart-on-fhir/fred" target="_blank">Github</a>. To stay updated on the project follow <a href="https://twitter.com/intent/user?screen_name=gotdan" target="_blank">@gotdan</a> and <a href="https://twitter.com/intent/user?screen_name=smarthealthit" target="_blank">@smarthealthit</a> on twitter.
+			<a href="https://github.com/PuraJuniper/fred">SAGE</a> is based on FRED, which is project of <a href="https://smarthealthit.org" target="_blank">SMART Health IT</a> and the open source code for the app is available on <a href="https://github.com/PuraJuniper/fred">GitHub</a>. To stay updated on the project follow <a href="https://twitter.com/intent/user?screen_name=gotdan" target="_blank">@gotdan</a> and <a href="https://twitter.com/intent/user?screen_name=smarthealthit" target="_blank">@smarthealthit</a> on twitter.
 			</div>
 		</div>;
 	}
