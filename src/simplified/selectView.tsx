9fcd73c051bf66beb35b192e0c4a47e8a37b9710
Joseph <joseph@junipercds.com>
2022-01-20 13:07:48 -0330

    Display as Saved Cards and New Card in basic view

--- src/simplified/selectView.tsx
+++ src/simplified/selectView.tsx
@@ -72,7 +72,7 @@ const SelectView = () => {
             <h3 className="col-lg-10 col-md-9" style={{color:"#2a6b92"}}><b>Make a Card</b></h3>
             <button className="navigate col-lg-2 col-md-3" 
                 onClick={() => State.get().set("ui", {status:"collection"})}>
-                Saved Resources&nbsp;<FontAwesomeIcon icon={faCaretRight} />
+                Saved Cards&nbsp;<FontAwesomeIcon icon={faCaretRight} />
                             
             </button>
             </div>
