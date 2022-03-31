ce42cd116d3c689b01a5af295efaf3deaea146a0
Joseph <joseph@junipercds.com>
2022-03-31 16:32:23 -0230

    slight styling still needed and code refactoring needed

--- src/simplified/selectView.tsx
+++ src/simplified/selectView.tsx
@@ -1,7 +1,7 @@
 import { BaseCard } from "./baseCard";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretRight, faInfoCircle } from '@fortawesome/pro-solid-svg-icons';
+import { faCaretRight, faInfoCircle,faGrid,faBookMedical,faCirclePlus } from '@fortawesome/pro-solid-svg-icons';
 import State from "../state";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import friendlyNames from "../../friendly-names.json";
@@ -40,52 +40,63 @@ const SelectView = () => {
             <Row lg="4" md="3" sm="2" noGutters>
                 <div style={{ padding: "10px" }} key={listOfHomePage[0].title}>
                     <Col>
-                        <h4>{listOfHomePage[0].title}</h4>
+                        <h4 style={{'fontSize':'10px'}}>{listOfHomePage[0].title}</h4>
                         <Card bg="sage-purple" text= {'white'} >
-                            <Card.Body>
+                            <Card.Body style={{'padding': '10'}}>
+                                <Row style={{'justifyContent': 'flex-end', 'margin':'0'}}>
+                                    <span style={{ fontSize: "20px", textAlign: "right" }}>
+                                        <a href='' target="_blank" rel="noreferrer" className="c-tooltip">
+                                            <FontAwesomeIcon icon={faInfoCircle} style={{'color':'white'}} />
+                                            <span className="c-tooltiptext">FHIR Docs</span>
+                                        </a>
+                                    </span>
+                                </Row>
                                 <Card.Title style={{ fontSize: "20px", textAlign: "center" }}>{listOfHomePage[0].cardText}</Card.Title>
-                                <span style={{ fontSize: "20px", textAlign: "right" }}>
-                                    <a href='' target="_blank" rel="noreferrer" className="c-tooltip">
-                                        <FontAwesomeIcon icon={faInfoCircle} />
-                                        <span className="c-tooltiptext">FHIR Docs</span>
-                                    </a>
-                                </span>
+                                <Row style={{'justifyContent': 'center', 'marginBottom':'30px'}}>
+                                    <FontAwesomeIcon icon={faCirclePlus} style={{'color':'white', 'height':'60px'}} />
+                                </Row>
                             </Card.Body>
                         </Card>
                     </Col>
                 </div>
                 <div style={{ padding: "10px" }} key={listOfHomePage[1].title}>
                     <Col>
-                        <h4>{listOfHomePage[1].title}</h4>
+                        <h4 style={{'fontSize':'10px'}}>{listOfHomePage[1].title}</h4>
                         <Card bg="sage-darkblue" text= {'white'} >
-                            <Card.Body style={{'padding': '0'}}>
+                            <Card.Body style={{'padding': '10'}}>
                                 <Row style={{'justifyContent': 'flex-end', 'margin':'0'}}>
                                     <span style={{ fontSize: "20px", textAlign: "right" }}>
                                         <a href='' target="_blank" rel="noreferrer" className="c-tooltip">
-                                            <FontAwesomeIcon icon={faInfoCircle} />
+                                            <FontAwesomeIcon icon={faInfoCircle} style={{'color':'white'}} />
                                             <span className="c-tooltiptext">FHIR Docs</span>
                                         </a>
                                     </span>
                                 </Row>
                                 <Card.Title style={{ fontSize: "20px", textAlign: "center" }}>{listOfHomePage[1].cardText}</Card.Title>
- 
+                                <Row style={{'justifyContent': 'center', 'marginBottom':'30px'}}>
+                                    <FontAwesomeIcon icon={faBookMedical} style={{'color':'white', 'height':'60px'}} />
+                                </Row>
                             </Card.Body>
                         </Card>
                     </Col>
                 </div>
-                <div style={{ padding: "10px" }} key={listOfHomePage[2].title}>
+                <div style={{ padding: "10px"}} key={listOfHomePage[2].title}>
                     <Col>
-                        <h4>{listOfHomePage[2].title}</h4>
-
+                        <h4 style={{'fontSize':'10px'}}>{listOfHomePage[2].title}</h4>
                         <Card bg="sage-green">
-                            <Card.Body>
+                            <Card.Body style={{'padding': '10px'}}>
+                                <Row style={{'justifyContent': 'flex-end', 'margin':'0'}}>
+                                    <span style={{ fontSize: "20px", textAlign: "right" }}>
+                                        <a href='' target="_blank" rel="noreferrer" className="c-tooltip">
+                                            <FontAwesomeIcon icon={faInfoCircle} style={{'color':'white'}} />
+                                            <span className="c-tooltiptext">FHIR Docs</span>
+                                        </a>
+                                    </span>
+                                </Row>
                                 <Card.Title style={{ fontSize: "20px", textAlign: "center" }}>{listOfHomePage[2].cardText}</Card.Title>
-                                <span style={{ fontSize: "20px", textAlign: "right" }}>
-                                    <a href='' target="_blank" rel="noreferrer" className="c-tooltip">
-                                        <FontAwesomeIcon icon={faInfoCircle} />
-                                        <span className="c-tooltiptext">FHIR Docs</span>
-                                    </a>
-                                </span>
+                                <Row style={{'justifyContent': 'center', 'marginBottom':'30px'}}>
+                                    <FontAwesomeIcon icon={faGrid} style={{'color':'white', 'height':'60px'}} />
+                                </Row>
                             </Card.Body>
                         </Card>
                     </Col>
