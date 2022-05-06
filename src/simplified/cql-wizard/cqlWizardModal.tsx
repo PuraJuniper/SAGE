21426a836cc01d7acdd66531028871f57de6667d
Raghavan Chandrabalan <raghavan.chandrabalan@purajuniper.com>
2022-05-06 15:03:22 -0400

    move all wizard sections to one page

--- src/simplified/cql-wizard/cqlWizardModal.tsx
+++ src/simplified/cql-wizard/cqlWizardModal.tsx
@@ -1,34 +1,11 @@
-import React, { useEffect, useReducer, useState, Dispatch } from "react";
-import { Button, Card, Modal, Pagination, Row } from "react-bootstrap";
+import React, { useEffect, useReducer } from "react";
+import { Button, Col, Container, Modal, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { CqlWizardSelectResource } from "./cqlWizardSelectResource";
-import { faArrowLeft, faArrowRight, faCheck } from "@fortawesome/pro-solid-svg-icons";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { faCheck } from "@fortawesome/pro-solid-svg-icons";
 import { CqlWizardSelectCodes } from "./cqlWizardSelectCodes";
 import { WizardAction, WizardPage, WizardReducer, WizardPagesArr, getNextPage, getPrevPage, initFromState, WizardState, StepStatus } from "./wizardLogic";
 import { CqlWizardSelectFilters } from "./cqlWizardSelectFilters";
-import { CSSTransitionStrictMode } from "../../helpers/CSSTransitionStrictMode";
-
-// Get the title for the dialog header and the page content for the dialog body
-function getTitleAndPage(wizardState: WizardState, wizardDispatch: Dispatch<WizardAction>): {title: string, pageComponent: JSX.Element} {
-    switch(wizardState.page) {
-        case WizardPage.SelectResource:
-            return {
-                title: "Which resource type do you want to search for?",
-                pageComponent: <CqlWizardSelectResource wizState={wizardState} wizDispatch={wizardDispatch} />
-            }
-        case WizardPage.SelectCodes:
-            return {
-                title: `${wizardState.resType}: Which codes do you want to select?`,
-                pageComponent: <CqlWizardSelectCodes wizState={wizardState} wizDispatch={wizardDispatch} />
-            }
-        case WizardPage.SelectFilters:
-            return {
-                title: `${wizardState.resType}: What restrictions should apply?`,
-                pageComponent: <CqlWizardSelectFilters wizState={wizardState} wizDispatch={wizardDispatch} />
-            }
-    }
-}
 
 interface CqlWizardModalProps {
     show: boolean,
@@ -60,31 +37,7 @@ export const CqlWizardModal: React.FunctionComponent<CqlWizardModalProps> = (pro
         wizardDispatch(["setState", initFromState(null)]);
     }
     
-    const {title: pageTitle, pageComponent: pageContent} = getTitleAndPage(wizardState, wizardDispatch);
-    
-    // Page items for header pagination
-    const paginationItems = WizardPagesArr.map((v) => 
-        <Pagination.Item key={v} active={page==v} id='cql-wizard-pagination-item'
-            disabled={pageStatus[v] === StepStatus.Disabled || pageStatus[v] === StepStatus.Skipped}
-            onClick={() => wizardDispatch(['changePage', v])}
-        >
-            <div className="cql-wizard-pagination-text">
-                {(()=>{
-                    switch(v) {
-                        case WizardPage.SelectResource:
-                            return "Select Resource";
-                        case WizardPage.SelectCodes:
-                            return "Select Codes";
-                        case WizardPage.SelectFilters:
-                            return "Select Restrictions";
-                    }
-                })()}
-            </div>
-        </Pagination.Item>
-    );
-    
-    const [canProceed, nextPage] = getNextPage(page, pageStatus);
-    const [canGoBack, prevPage] = getPrevPage(page, pageStatus);
+    const [canSubmit] = getNextPage(WizardPage.SelectFilters, pageStatus);
     return (
         <Modal
             show={props.show}
@@ -93,46 +46,30 @@ export const CqlWizardModal: React.FunctionComponent<CqlWizardModalProps> = (pro
             centered
             animation={true}
             contentClassName="cql-wizard-modal"
+            backdrop="static"
         >
-            <Modal.Header>
-                <div className="cql-wizard-header-content">
-                    <Pagination className="cql-wizard-pagination">
-                        {paginationItems}
-                    </Pagination>
-                    <Modal.Title>{pageTitle}</Modal.Title>
-                </div>
+            <Modal.Header closeButton>
+                <Modal.Title>Create a condition</Modal.Title>
             </Modal.Header>
             <Modal.Body className="cql-wizard-modal-body">
-                <div className="cql-wizard-body-content">
-                    <Button variant="light" className="cql-wizard-nav-button" disabled={!canGoBack}
-                        onClick={() => {
-                            if (prevPage) {
-                                wizardDispatch(['changePage', prevPage])
-                            }
-                        }}
-                    >
-                        <FontAwesomeIcon icon={faArrowLeft} />
-                    </Button>
-                    <div className="cql-wizard-page-content-closest-positioned-ancestor">
-                        <TransitionGroup component={null}>
-                            <CSSTransitionStrictMode classNames="cql-wizard-content-transition" key={page} timeout={300}>
-                                <div className="cql-wizard-page-content">
-                                    {pageContent}
-                                </div>
-                            </CSSTransitionStrictMode>
-                        </TransitionGroup>
-                    </div>
-                    <Button variant={nextPage === null && canProceed ? "success" : "light"} className="cql-wizard-nav-button" disabled={!canProceed}
-                        onClick={() => {
-                            if (nextPage === null) {
-                                handleSaveAndClose();
-                            }
-                            else {
-                                wizardDispatch(['changePage', nextPage])
-                            }
-                        }}
-                    >
-                        <FontAwesomeIcon icon={nextPage === null ? faCheck : faArrowRight} />
+                <Container className="cql-wizard-body-content" fluid>
+                    <Row className="gy-4">
+                        <Col xs={12}>
+                            <b>Which resource are you searching for?</b>
+                            <CqlWizardSelectResource wizState={wizardState} wizDispatch={wizardDispatch} />
+                        </Col>
+                        <Col xs={12}>
+                            <CqlWizardSelectCodes wizState={wizardState} wizDispatch={wizardDispatch} />
+                        </Col>
+                        <Col xs={12}>
+                            <CqlWizardSelectFilters wizState={wizardState} wizDispatch={wizardDispatch} />
+                        </Col>
+                    </Row>
+                </Container>
+                <div className="cql-wizard-floating-submit shadow">
+                    <Button disabled={!canSubmit} onClick={handleSaveAndClose}>
+                        <FontAwesomeIcon icon={faCheck} style={{ marginRight: "0.5rem" }} />
+                        Save Condition
                     </Button>
                 </div>
             </Modal.Body>
