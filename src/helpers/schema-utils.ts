dce4f90a4e10f6ab456039f7e4e98e8bbf9951f7
Josh Liben <josh@junipercds.com>
2022-05-17 15:49:35 -0400

    merging from dev fixed the related-aritfact test

--- src/helpers/schema-utils.ts
+++ src/helpers/schema-utils.ts
@@ -135,9 +135,6 @@ export function toFhir(decorated: SageNodeInitialized, validate: boolean): [Sage
 				} else if (["valueArray"].includes(child.nodeType)) {
 					return _walkNode(child, []);
 				} else if (["objectArray"].includes(child.nodeType)) {
-					// Uncomment this line to bring back the behavior that fixed the relatedArtifact export error, 
-					//  but note that it will break some tests in schema-utils.test.ts and thus the CI deployment
-					// return _walkNode(child, [{}]);
 					return _walkNode(child, []);
 				} else {
 					let err;
